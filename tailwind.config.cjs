b1c856adb3de1f1213944b07d26dee34e9f43664
kiliancabrera <kilian.cabrera@qida.es>
2023-01-28 17:35:36 +0100

    First portfolio version

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        pinkGradient: "#E70FAA",
+        blueGradient: "#00C0FD",
+      },
+    },
+  },
+  plugins: [],
+};
