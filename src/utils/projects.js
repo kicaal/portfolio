d2975fcfa92fb0236bc8ffe0d96bc90c93c8f023
Kilian Cabrera <kilianbboy@gmail.com>
2023-06-04 18:23:07 +0200

    Added projects

---
+++ src/utils/projects.js
@@ -0,0 +1,24 @@
+import infojobsHackaton from "../assets/infojobs-hackaton.png";
+import Game from "../assets/2048game.png";
+import Giffy from "../assets/giffy.png";
+
+export const projects = [
+  {
+    title: "AI job offer generator",
+    href: "https://kilian-hackaton-adevinta.vercel.app/",
+    image: infojobsHackaton,
+    publishedAt: "01/06/2023",
+  },
+  {
+    title: "2048 game",
+    href: "https://2048-game-kilian.vercel.app",
+    image: Game,
+    publishedAt: "26/02/2023",
+  },
+  {
+    title: "Giffy Searcher",
+    href: "https://giffy-react-searcher.vercel.app",
+    image: Giffy,
+    publishedAt: "04/10/2021",
+  },
+];
