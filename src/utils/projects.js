a736234449539b9cd73ff25656dcb85e675e1f1c
Kilian Cabrera <kilianbboy@gmail.com>
2023-06-05 17:32:44 +0200

    Added new project

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,8 +1,15 @@
 import infojobsHackaton from "../assets/infojobs-hackaton.png";
 import Game from "../assets/2048game.png";
 import Giffy from "../assets/giffy.png";
+import Netflix from "../assets/netflix-clone.png";
 
 export const projects = [
+  {
+    title: "Netflix Clone / Github and Google OAuth",
+    href: "https://netflix-clone-kicaal.vercel.app/",
+    image: Netflix,
+    publishedAt: "05/06/2023",
+  },
   {
     title: "AI job offer generator",
     href: "https://kilian-hackaton-adevinta.vercel.app/",
